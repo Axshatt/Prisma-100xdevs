18719556f2de2d4ae1e9872b8103e765f52bca58
Akshat <codepreneur.in@gmail.com>
2025-07-29 22:34:27 +0530

    new usage

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,42 @@ import { PrismaClient } from "./generated/prisma";
 
 const client = new PrismaClient();
 
-client.user.create({
-    data:{
-        username:"Harkirat",
-        password:"123123",
-        age:21,
-        city:"Delhi"
-    }
-})
+
+async function createUser() {
+    // create
+    client.user.create({
+        data: {
+            username: "Harkirat",
+            password: "123123",
+            age: 21,
+            city: "Delhi"
+        }
+    })
+}
+
+async function updateUser() {
+    //update
+    client.user.update({
+        where: {
+            id: 1,
+        },
+        data: {
+            username: "Harkirat",
+        }
+    })
+}
+async function FindUser() {
+
+    // reading data ot user/users
+    const response = await client.user.findFirst({
+        where: {
+            id: 1,
+        }
+
+    })
+    console.log(response?.password);
+}
 
 
+
+createUser()
